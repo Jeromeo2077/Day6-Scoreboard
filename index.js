d73159fafdb16346d5b8d01b1b09f0a3984b6b25
Jerome Otto <jeromeo1969@hotmail.com>
2024-08-19 16:16:13 -0600

    Finished incrementing of team scores

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ console.log('Hello World')
 let redTeamScore = 0
 let blueTeamScore = 0
 
+//Enable Teams Scores to Display the Initial Score of Zero
+document.getElementById("redTeamScore").innerHTML = redTeamScore
+document.getElementById("blueTeamScore").innerHTML = blueTeamScore
+
 //Reset Function
 
 function resetTeamScores() {
 
   redTeamScore = 0
   blueTeamScore = 0
+
+  document.getElementById("redTeamScore").innerHTML = redTeamScore
+  document.getElementById("blueTeamScore").innerHTML = blueTeamScore
 }
 
 //Score Incremental Functions
